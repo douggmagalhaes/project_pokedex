31dbb644879538997130ad367c2639acb7b42729
douggmagalhaes <dougxblack@gmail.com>
2023-11-15 20:39:38 -0300

    feature: install next, eslint, prettier and configs

---
+++ my-project-pokedex/src/pages/index.tsx
@@ -0,0 +1,17 @@
+//import Head from 'next/head'
+//import Image from "next/image";
+//import { Inter } from "next/font/google";
+//import styles from "@/styles/Home.module.css";
+
+//const inter = Inter({ subsets: ["latin"] });
+//className={`${styles.main} ${inter.className}`}
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <h1>home</h1>
+      </main>
+    </>
+  );
+}
